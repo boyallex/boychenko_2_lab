48060c5c5b6e18aad5c8748e5e24de26f5cf0b80
boyall <fbenkov@yandex.ru>
2022-11-14 22:51:49 +0300

    Update boychenko_2_lab.dart

--- lib/boychenko_2_lab.dart
+++ lib/boychenko_2_lab.dart
@@ -1,9 +1,58 @@
 import 'dart:io';
 import "dart:math";
 
-int countOfEqualStates() {
-  
+// int countOfEqualStates() {
+
+// }
+List<List<int>> createEqualMatrix(List<List<int>> matrix) {
+  List<List<int>> eq_matrix = matrix;
+  int i = 0; // строка состояния, которое мы проверяем
+  int j = 0;
+  bool isNotEqualRow;
+  while (i < eq_matrix.length) {
+    List<int> tempRow = eq_matrix[i];
+    j = i;
+    isNotEqualRow = true;
+    while (isNotEqualRow | (j < eq_matrix.length)) {
+      isNotEqualRow = isEqualLists(tempRow, eq_matrix[j]);
+      j++;
+    }
+    if (j == eq_matrix.length) {
+      // если прошли полностью, значит состояние не изоморфно
+      i++;
+    } else {
+      // если изоморфно, то удаляем второе, переименовываем и проходимся заново
+      int row = 0, column = 0;
+      eq_matrix.remove(eq_matrix[j]); // удаляем строку состояния
+      while (row < eq_matrix.length) {
+        while (column < eq_matrix[row].length) {
+          if ((column == j)) {
+            eq_matrix[row].remove(eq_matrix[row][column]); // удаляем элемент
+          }
+          if (eq_matrix[row][column] == j) {
+            eq_matrix[row][column] = i;
+          }
+          column++;
+        }
+        row++;
+      }
+      i = 0; // чтобы пройтись с начала обнуляем счетчик
+    }
+  }
+  return eq_matrix;
 }
+
+bool isEqualLists(List<int> listA, List<int> listB) {
+  if (listA.length != listB.length) {
+    return false;
+  }
+  int i = 0;
+  while ((listA[i] == listB[i]) | (i < listA.length)) {
+    i++;
+  }
+  return i == listA.length;
+}
+
 List<int> findTransientStates(List<List<String>> matrix) {
   final int len = matrix.length;
   int j;
@@ -36,6 +85,7 @@ void printMatrix(List<List<int>> matrix) {
     stdout.write("\n");
   }
 }
+
 List<List<int>> createRanodmMatrix(int n) {
   List<List<int>> matrix = [];
 
